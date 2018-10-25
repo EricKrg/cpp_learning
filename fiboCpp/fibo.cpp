ac38671f2c884a454ad2fc4f310d74a7e1bccda3
EricKrg <eric.krueger@uni-jena.de>
2018-10-25 11:31:48 +0200

    repo baseline

---
+++ fiboCpp/fibo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <chrono>
+#include <cstdlib>
+
+using std::chrono::system_clock;
+using std::chrono::duration;
+using std::vector;
+using namespace std;
+//----------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------
+int main(int argc, char** argv) {
+    // Interactive input
+    if(argc !=  2){
+        cout<<"no/not enough arguments given"<<endl;
+        return 1;
+    } else {
+        return 0;
+    }
+}
